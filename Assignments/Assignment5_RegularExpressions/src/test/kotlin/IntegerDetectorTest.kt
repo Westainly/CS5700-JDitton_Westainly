e6d9e419eb5293e1adc06f7a796aee7d9c5c583a
Weston Johnson <a02252990@aggies.usu.edu>
2024-08-05 22:02:04 -0600

    Changes layout in integer testing for better grouping

--- Assignments/Assignment5_RegularExpressions/src/test/kotlin/IntegerDetectorTest.kt
+++ Assignments/Assignment5_RegularExpressions/src/test/kotlin/IntegerDetectorTest.kt
@@ -8,11 +8,12 @@ internal class IntegerDetectorTest {
     fun detect() {
         val detector = IntegerDetector()
         assertTrue(detector.detect("123"))
-        assertFalse(detector.detect("0123"))
-        assertFalse(detector.detect("123a4"))
         assertTrue(detector.detect("10983275409812374012938"))
         assertTrue(detector.detect("1"))
+        
         assertFalse(detector.detect("0"))
         assertFalse(detector.detect(""))
+        assertFalse(detector.detect("0123"))
+        assertFalse(detector.detect("123a4"))
     }
 }
